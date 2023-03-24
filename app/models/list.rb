77f4352d9f3d853bb057d8a0096ec482548ff19e
Anhnhi Tran <anhtran811@gmail.com>
2023-03-24 16:03:43 -0600

    Formatting changes

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,4 @@
 class List < ApplicationRecord
   has_many :list_items, dependent: :destroy
   has_many :items, through: :list_items, dependent: :destroy
-  # belongs_to :user, through: :items
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe List, type: :model do
   describe 'relationships' do
+    it { should belong_to(:user).through(:item) }
     it { should have_many(:list_items) }
     it { should have_many(:items).through(:list_items) }
   end
