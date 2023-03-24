4846d202c64e79d950d777d9caa99e414c62d737
Anhnhi Tran <anhtran811@gmail.com>
2023-03-24 16:03:16 -0600

    Added enums to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :list_items, dependent: :destroy
   has_many :lists, through: :list_items, dependent: :destroy
+  
+  enum season: ["all season", "spring", "summer", "fall", "winter"]
+  enum clothing_type: ["other", "tops", "bottoms", "shoes", "accessories", "outerwear"]
+  enum color: ["unspecified", "red", "orange", "yellow", "green", "blue", "purple", "black", "white", "neutral", "multi"]
 end
