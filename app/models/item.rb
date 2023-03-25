9316f8e64ba9e4888fac074fd2dd3070dc9e6c36
Kelsie Porter <krporter22@gmail.com>
2023-03-25 11:47:51 -0600

    Remove image_url column and tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,11 @@ class Item < ApplicationRecord
   has_many :list_items, dependent: :destroy
   has_many :lists, through: :list_items, dependent: :destroy
 
-  validates_presence_of :clothing_type, :season, :color, :image_url
+  validates_presence_of :clothing_type, :season, :color
   
   enum season: ["all season", "spring", "summer", "fall", "winter"]
   enum clothing_type: ["other", "tops", "bottoms", "shoes", "accessories", "outerwear"]
   enum color: ["unspecified", "red", "orange", "yellow", "green", "blue", "purple", "black", "white", "neutral", "multi"]
+
+  has_one_attached :image, service: :s3
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_25_161852023_03_24_215510) do
+ActiveRecord::Schema.define(version: 2023_03_25_174558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 2023_03_25_161852023_03_24_215510) do
     t.integer "clothing_type", default: 0
     t.string "size"
     t.integer "color", default: 0
-    t.string "image_url"
     t.string "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -70,8 +69,8 @@ ActiveRecord::Schema.define(version: 2023_03_25_161852023_03_24_215510) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
   add_foreign_key "list_items", "items"
   add_foreign_key "list_items", "lists"
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
