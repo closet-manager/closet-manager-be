f649830967b37f987ebfb5833d85db747845fa88
Braxton Eddings <beddings81@gmail.com>
2023-03-28 17:59:22 -0500

    Add model test for filtering items

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,10 +13,36 @@ class Item < ApplicationRecord
   enum season: ["all season", "spring", "summer", "fall", "winter"]
   enum clothing_type: ["other", "tops", "bottoms", "shoes", "accessories", "outerwear"]
   enum color: ["unspecified", "red", "orange", "yellow", "green", "blue", "purple", "black", "white", "neutral", "multi"]
-
+  
+  # enum season: [:all_season, "spring", "summer", "fall", "winter"]
   has_one_attached :image # , service: :s3
 
   def image_url
     url_for(image)
+  end
+
+  def self.filter_by(season=nil, clothing_type=nil, color=nil)
+    if clothing_type.nil? && color.nil?
+      self
+      .where("season = ?", "#{season}")
+    elsif season.nil? && color.nil?
+      self
+      .where("clothing_type = ?", "#{clothing_type}")
+    elsif season.nil? && clothing_type.nil?
+      self
+      .where("color = ?", "#{color}")
+    elsif color.nil?
+      self
+      .where("season = ? AND clothing_type = ?", "#{season}", "#{clothing_type}")
+    elsif clothing_type.nil?
+      self
+      .where("season = ? AND color = ?", "#{season}", "#{color}")
+    elsif season.nil?
+      self
+      .where("clothing_type = ? AND color = ?", "#{clothing_type}", "#{color}")
+    else
+      self
+      .where("season = ? AND clothing_type = ? AND color = ?", "#{season}", "#{clothing_type}", "#{color}")
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,46 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of(:season) }
     it { should validate_presence_of(:color) }
   end
+
+  describe 'class methods' do
+    describe '#filter_by' do
+      it 'can filter a users items' do
+        user = create(:user)
+
+        item1 = Item.create!(user_id: user.id, season: "spring", clothing_type: "tops", size: "L", color: "orange")
+        item2 = Item.create!(user_id: user.id, season: "spring", clothing_type: "shoes", size: "L", color: "black")
+        item3 = Item.create!(user_id: user.id, season: "fall", clothing_type: "bottoms", size: "L", color: "blue")
+        item4 = Item.create!(user_id: user.id, season: "fall", clothing_type: "bottoms", size: "L", color: "blue")
+        item5 = Item.create!(user_id: user.id, season: "fall", clothing_type: "tops", size: "L", color: "orange")
+
+        #season
+        expect(user.items.filter_by(1)).to eq([item1, item2])
+        expect(user.items.filter_by(4)).to eq([])
+
+        #clothing_type
+        expect(user.items.filter_by(nil, 2)).to eq([item3, item4])
+        expect(user.items.filter_by(nil, 4)).to eq([])
+
+        #color
+        expect(user.items.filter_by(nil, nil, 2)).to eq([item1, item5])
+        expect(user.items.filter_by(nil, nil, 0)).to eq([])
+
+        #season && clothing_type && color
+        expect(user.items.filter_by(3, 2, 5)).to eq([item3, item4])
+        expect(user.items.filter_by(3, 2, 0)).to eq([])
+
+        #season && clothing_type
+        expect(user.items.filter_by(3, 1, nil)).to eq([item5])
+        expect(user.items.filter_by(3, 0, nil)).to eq([])
+
+        #season && color
+        expect(user.items.filter_by(3, nil, 5)).to eq([item3, item4])
+        expect(user.items.filter_by(3, nil, 0)).to eq([])
+
+        #clothing_type && color
+        expect(user.items.filter_by(nil, 2, 5)).to eq([item3, item4])
+        expect(user.items.filter_by(nil, 0, 5)).to eq([])
+      end
+    end
+  end
 end
