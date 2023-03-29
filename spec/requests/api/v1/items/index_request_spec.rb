21810aaf9c5ce4b73751599514a1d1b902ec6906
Kelsie Porter <krporter22@gmail.com>
2023-03-29 17:35:30 -0600

    Add tests for get a users items

--- spec/requests/api/v1/items/index_request_spec.rb
+++ spec/requests/api/v1/items/index_request_spec.rb
@@ -19,13 +19,51 @@ describe 'GET /users/:id/items' do
         item.image.attach(blob)
       end
 
-      get "/api/v1/users/#{user.id}"
+      get "/api/v1/users/#{user.id}/items"
 
       items_response = JSON.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
+
       expect(response).to be_successful
       expect(response.status).to eq(200)
-      #item4 does NOT exist
+
+      expect(items_response).to have_key(:data)
+      expect(items_response[:data]).to be_an(Array)
+      expect(items_response[:data].length).to eq(3)
+
+      items_response[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item).to have_key(:type)
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes]).to have_key(:user_id)
+        expect(item[:attributes][:user_id]).to be_a(Integer)
+        expect(item[:attributes]).to have_key(:season)
+        expect(item[:attributes][:season]).to be_a(String)
+        expect(item[:attributes]).to have_key(:clothing_type)
+        expect(item[:attributes][:clothing_type]).to be_a(String)
+        expect(item[:attributes]).to have_key(:size)
+        expect(item[:attributes][:size]).to eq(nil).or be_a(String)
+        expect(item[:attributes]).to have_key(:image_url)
+        expect(item[:attributes][:image_url]).to be_a(String)
+        expect(item[:attributes]).to have_key(:notes)
+        expect(item[:attributes][:notes]).to eq(nil).or be_a(String)
+      end
+    end
+  end
+
+  context 'if the user does not have items' do 
+    it 'returns an empty array' do 
+      user = create(:user)
+
+      get "/api/v1/users/#{user.id}/items"
+
+      items_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      expect(items_response).to have_key(:data)
+      expect(items_response[:data]).to be_an(Array)
+      expect(items_response[:data].length).to eq(0)
     end
   end
 end
