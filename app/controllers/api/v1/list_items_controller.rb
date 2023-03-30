569b2f68e999220f5b7fa15c1d8736bfabe1ce2b
Anhnhi Tran <anhtran811@gmail.com>
2023-03-30 15:11:33 -0600

    Formatting changes

--- app/controllers/api/v1/list_items_controller.rb
+++ app/controllers/api/v1/list_items_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::ListItemsController < ApplicationController
     list = List.find(params[:list_id])
     
     ListItem.create!(item_id: item.id, list_id: list.id)
+
     render json: { message: "Item has been successfully added to #{list.name}" }, status: 201
   end
 end
