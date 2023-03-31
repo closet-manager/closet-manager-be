41a484c2c1d7c6c961c716d63536062a3ec0fb72
Braxton Eddings <beddings81@gmail.com>
2023-03-30 20:58:36 -0500

    Add sad path test for item creation

--- spec/requests/api/v1/items/new_request_spec.rb
+++ spec/requests/api/v1/items/new_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'POST /users/:id/items' do
-  describe 'if the image is not included' do
+  describe 'if a user exists and the image is not included' do
     it 'create an item and attach a default image' do
       user = create(:user)
 
@@ -58,6 +58,41 @@ describe 'POST /users/:id/items' do
 
       expect(item_response[:data][:attributes]).to have_key(:notes)
       expect(item_response[:data][:attributes][:notes]).to be_a(String)
+    end
+  end
+
+  describe 'if a user does not exists' do
+    it 'returns an error message' do
+
+      headers = { "CONTENT_TYPE": "application/json"}
+      item_params = {
+                      "user_id": 1,
+                      "season": "all_season",
+                      "clothing_type": "shoes",
+                      "color": "black",
+                      "size": "7",
+                      "image": nil,
+                      "notes": "testing header"
+                    }
+
+      expect(Item.count).to eq(0)
+
+      post "/api/v1/users/1/items", headers: headers, params: JSON.generate(item_params)
+
+      item_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(Item.count).to eq(0)
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      expect(item_response).to have_key(:error)
+      expect(item_response[:error]).to be_a(Array)
+
+      expect(item_response[:error][0]).to have_key(:title)
+      expect(item_response[:error][0][:title]).to be_a(String)
+
+      expect(item_response[:error][0]).to have_key(:status)
+      expect(item_response[:error][0][:status]).to be_a(String)
     end
   end
 end
