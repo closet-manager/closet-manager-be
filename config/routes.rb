c251f48b0f94c108f807e0e19c3b9a1628f0cce3
Anhnhi Tran <anhtran811@gmail.com>
2023-03-31 14:19:43 -0600

    Fixed route to reflect endpoint change

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
         get '/items/find_all', to: 'items/search#show'
         resources :items, only: [:show, :create, :update, :index]
         resources :lists, only: [:index, :show, :create]
-        # resources :list_items, only: [:destroy]
       end
       resources :items, only: [] do
         resources :lists, only: [:destroy], controller: :list_items do
