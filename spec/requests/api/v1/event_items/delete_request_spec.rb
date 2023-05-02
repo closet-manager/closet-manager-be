84c09094c5c6e0c7a33c265a43bae0f681d18991
Anhnhi Tran <anhtran811@gmail.com>
2023-05-01 19:24:33 -0600

    Completed sad path testing for deleting an item from an event

--- spec/requests/api/v1/event_items/delete_request_spec.rb
+++ spec/requests/api/v1/event_items/delete_request_spec.rb
@@ -28,4 +28,47 @@ describe 'DELETE /items/:item_id/date' do
       expect(User.count).to eq(1)
     end
   end
+
+  context 'when the item does not exists' do
+    it 'returns an error message' do
+      event = Event.create!(outfit_date: Date.today)
+
+      delete "/api/v1/items/9999999999999999/#{event.outfit_date}"
+
+      event_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      expect(event_response).to have_key(:error)
+      expect(event_response[:error]).to be_a(Array)
+      expect(event_response[:error][0]).to have_key(:title)
+      expect(event_response[:error][0][:title]).to be_a(String)
+      expect(event_response[:error][0]).to have_key(:status)
+      expect(event_response[:error][0][:status]).to be_a(String)
+    end
+  end
+
+  context 'when the date does not exists' do
+    it 'returns an error message' do
+      user = create(:user)
+      event = Event.create!(outfit_date: Date.today)
+      item1 = Item.create(user_id: user.id, clothing_type: "tops")
+      event_item1 = EventItem.create!(event_id: event.id, item_id: item1.id)
+
+      delete "/api/v1/items/#{item1.id}/#{Date.today+1.day}"
+
+      event_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      expect(event_response).to have_key(:error)
+      expect(event_response[:error]).to be_a(Array)
+      expect(event_response[:error][0]).to have_key(:title)
+      expect(event_response[:error][0][:title]).to be_a(String)
+      expect(event_response[:error][0]).to have_key(:status)
+      expect(event_response[:error][0][:status]).to be_a(String)
+    end
+  end
 end
