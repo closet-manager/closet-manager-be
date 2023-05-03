92ac2757c4871ca7448b43160b5f3a57c9c29416
Braxton Eddings <beddings81@gmail.com>
2023-05-03 17:45:49 -0500

    Update the event index endpoint to include events with one or more items

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::EventsController < ApplicationController
   def index
-    events = Event.all
+    events = Event.all_events_with_items
     render json: EventSerializer.new(events)
   end
 
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Event, type: :model do
     it { should validate_presence_of(:outfit_date) }
     it { should validate_uniqueness_of(:outfit_date) }
   end
+
+  describe 'class methods' do
+    describe '#find_all_events_with_items' do
+      it 'returns all events that have items' do
+        user = create(:user)
+        item1 = Item.create(user_id: user.id, clothing_type: "tops")
+        event1 = Event.create!(outfit_date: Date.today)
+        event2 = Event.create!(outfit_date: Date.today+1.day)
+        event3 = Event.create!(outfit_date: Date.today+2.day)
+        event_item1 = EventItem.create!(event_id: event1.id, item_id: item1.id)
+        event_item2 = EventItem.create!(event_id: event2.id, item_id: item1.id)
+
+        expect(Event.all_events_with_items).to eq([event1, event2])
+        expect(Event.all_events_with_items).to_not include([event3])
+      end
+    end
+  end
 end
